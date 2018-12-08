1f05d4f1e692c98101676ca19f679cc9ce98db87
Oscar Vuong <oscarvuong@gmail.com>
2018-12-08 03:14:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,23 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+  def self.find_by_name(song_name)
+    s
+
+
 end
